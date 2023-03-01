d5e7508c00379390ae96e54252e0f95ee2e672f5
jin.zhong <z206674576@163.com>
2023-03-01 16:22:15 +0800

    feat:

--- components/button/index.ts
+++ components/button/index.ts
@@ -1,6 +1,6 @@
 import type { App } from "vue";
 
-import Button from "./src/index"
+import Button from "./src/index.vue"
 
 Button.install = (app: App) => app.component(Button.name, Button);
 export default Button;
